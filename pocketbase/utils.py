38b23b7281163dd098da9bc40d1ed1851f1d330d
Jordan GARAY <jordan.garay.39@gmail.com>
2025-06-25 15:23:54 +0200

    Désactiver la fonction camel_to_snake

--- pocketbase/utils.py
+++ pocketbase/utils.py
@@ -3,14 +3,15 @@ from __future__ import annotations
 import base64
 import datetime
 import json
-import re
+#  import re
 
 from .errors import ClientResponseError  # noqa: F401
 
 
 def camel_to_snake(name: str) -> str:
-    name = re.sub("(.)([A-Z][a-z]+)", r"\1_\2", name)
-    return re.sub("([a-z0-9])([A-Z])", r"\1_\2", name).lower()
+    return name  # deactivate this line to use the original function
+    #  name = re.sub("(.)([A-Z][a-z]+)", r"\1_\2", name)
+    #  return re.sub("([a-z0-9])([A-Z])", r"\1_\2", name).lower()
 
 
 def to_datetime(
